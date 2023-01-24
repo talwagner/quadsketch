c83e2141816a6f636a46aacc80e2cdac1d2044f8
removedporn <86824510+removedporn@users.noreply.github.com>
2023-01-24 16:09:25 +0800

    Delete datasets directory

--- datasets/raw/glove/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf glove.twitter.27B.* dataset_unnormalized.dat dataset.dat queries.dat answers.dat step1 step2
--- datasets/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-aws s3 cp glove.tar.gz s3://sketches-datasets/
-aws s3 cp mnist.tar.gz s3://sketches-datasets/
-aws s3 cp sift.tar.gz s3://sketches-datasets/
-aws s3 cp sift_subsample.tar.gz s3://sketches-datasets/
-
